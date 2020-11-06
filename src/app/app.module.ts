6bb002f23aeab4e75078c81a75d6b774fbd19277
ivakm <sheva594@ukr.net>
2020-11-06 23:03:34 +0200

    added interface for last-news in the previously commit Got setting short import ways

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {HeaderComponent} from './shared/modules/header/header.component';
-import {FooterComponent} from './shared/modules/footer/footer.component';
-import {MainComponent} from './page/main/main.component';
+import {HeaderComponent} from '@shared/modules/header/header.component';
+import {FooterComponent} from '@shared/modules/footer/footer.component';
+import {MainComponent} from '@page/main/main.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
@@ -16,11 +16,11 @@ import {MatMenuModule} from '@angular/material/menu';
 import {MatSelectModule} from '@angular/material/select';
 import {FormsModule} from '@angular/forms';
 import {NgxAudioPlayerModule} from 'ngx-audio-player';
-import {NewsComponent} from './page/news/news.component';
+import {NewsComponent} from '@page/news/news.component';
 import {HttpClientModule} from '@angular/common/http';
 import {TranslocoRootModule} from './transloco/transloco-root.module';
-import { PopularNewsComponent } from './page/main/popular-news/popular-news.component';
-import {LastNewsModule} from './shared/modules/last-news/last-news.module';
+import { PopularNewsComponent } from '@page/main/popular-news/popular-news.component';
+import {LastNewsModule} from '@shared/modules/last-news/last-news.module';
 
 @NgModule({
   declarations: [
